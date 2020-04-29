e1abf4874141b910c01123968081b7c5c27e5c9a
Toufiq <burningraven06@outlook.com>
2020-04-29 14:06:15 +0600

    Handle URL Routing

--- tweets/views.py
+++ tweets/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
+      print(args, kwargs)
       return HttpResponse("<h1>Hey World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+      print(args, kwargs)
+      return HttpResponse(f"<h1>Tweet Detail View {tweet_id}</h1>")
