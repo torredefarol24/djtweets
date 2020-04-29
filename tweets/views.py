d85693c730ce0aecd276225d3d65e296f46ee5a5
Toufiq <burningraven06@outlook.com>
2020-04-29 14:35:25 +0600

    Send JSON Response

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
 def home_view(request, *args, **kwargs):
       print(args, kwargs)
@@ -7,4 +8,19 @@ def home_view(request, *args, **kwargs):
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
       print(args, kwargs)
-      return HttpResponse(f"<h1>Tweet Detail View {tweet_id}</h1>")
+      try :
+            tweet = Tweet.objects.get(id=tweet_id)
+      except:
+            raise Http404
+
+      context = {
+            "success" : True,
+            "message" : "Tweets Found",
+            "data" : {
+                  "id" : tweet.id,
+                  "content" : tweet.content
+            }
+      }
+
+      # return HttpResponse(f"<h1>Tweet Detail View TweetID:{tweet_id} Content:{tweet.content}</h1>")
+      return JsonResponse(context)
