ee5d20a9e1ba1292650121f91aa75ea2aa2885d6
Toufiq <burningraven06@outlook.com>
2020-04-29 13:48:27 +0600

    Connect to  postgres DB

--- djtweet/settings.py
+++ djtweet/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweets'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'djtweet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "djtweet",
+        'USER': "postgres",
+        'PASSWORD' : "test1234",
+        'HOST': "localhost",
+        'POST': "5432"
     }
 }
 
---
+++ todo.md
@@ -0,0 +1,23 @@
+1. Tweets
+      -> Creating
+            -> Text
+            -> Image
+
+      -> Delete
+      -> Retweeting
+
+1. Useres
+
+      -> Register
+      -> Login
+      -> Logout
+      -> Profile
+            -> Image
+            -> Text
+            -> Follow Button
+      -> Feed
+            -> User's Feed
+            -> User + Who they Follow
+
+
+3. Following / Followers
